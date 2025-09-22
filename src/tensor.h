f31542442b904728e76da86ae055e027519d7a1b
Liam Ford <liam7ford@gmail.com>
2025-09-21 20:52:34 -0600

    matrix operation function sigs in tensor.h

--- src/tensor.h
+++ src/tensor.h
@@ -14,6 +14,20 @@ class Tensor {
         float getValue(int row, int col);
         void setValue(int row, int col, float value);
         void display();
+
+        Tensor matmul(const Tensor& other) const;
+        Tensor add(const Tensor& other) const;
+        Tensor subtract(const Tensor& other) const;
+        Tensor elementwise(const Tensor& other) const;
+
+        Tensor transpose() const;
+        Tensor softmax() const;
+        Tensor relu() const;
+        void fill(float value);
+        Tensor scale(float scaler) const;
+        
+        int getRows() const { return rows; }
+        int getCols() const { return cols; }
 };
 
 #endif
